0a54a575bf495c6678ff7a2a008492607886e81b
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-02-19 17:57:02 +0530

    do while loop

---
+++ week 1/day 3/src/lec_5_do_while_loop.java
@@ -0,0 +1,16 @@
+public class lec_5_do_while_loop {
+
+    public static void main(String[] args) {
+        
+        int i=1;
+        // i= 5;
+        // i= 10;
+
+        do{
+            System.out.println("do while loop = "+i);
+            i++;
+        }while(i<=5);
+
+    }
+    
+}
